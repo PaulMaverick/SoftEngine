2345ed71a115e8e5ae00e95e066517312be604f8
Paul <paulpastor122297@gmail.com>
2024-12-27 16:50:59 +0800

    added initial softEngine.js

---
+++ SoftEngine.js
@@ -0,0 +1,29 @@
+var SoftEngine;
+
+(function (SoftEngine) {
+    const Camera = (function () {
+        function Camera() {
+            this.Position = BABYLON.Vector3.Zero();
+            this.target = BABYLON.Vector3.Zero();
+        }
+        return Camera
+    })();
+
+    SoftEngine.Camera = Camera;
+
+    const Mesh = (function() {
+        function Mesh(name, verticesCount) {
+            this.name = name
+            this.Vertices = new Array(verticesCount);
+            this.Rotation = BABYLON.Vector3.Zero();
+            this.Position = BABYLON.Vector3.Zero();
+
+        }
+
+        return Mesh;
+    })();
+
+    SoftEngine.Mesh = Mesh;
+
+   
+})(SoftEngine || (SoftEngine = {}))
